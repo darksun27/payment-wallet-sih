6554736c31bd0c8f24a0a3321d126f33312ed104
darksun27 <sbsiddharth@gmail.com>
2020-01-25 00:21:10 +0530

    Payment Wallet Initial

--- index.js
+++ index.js
@@ -167,6 +167,10 @@ app.post("/api/add-amount", async (req, res)=> {
     res.send("Served");
 });
 
+app.get("/", (req, res)=>{
+    res.send("Hello World");
+})
+
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("Server Running");
 });
