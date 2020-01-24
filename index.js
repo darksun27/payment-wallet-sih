270332f6a5a5e39b892f9c267f2be49b10e9baec
darksun27 <sbsiddharth@gmail.com>
2020-01-25 01:56:20 +0530

    Payment Wallet Initial

--- index.js
+++ index.js
@@ -4,23 +4,30 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended : true}));
-mongoose.connect('mongodb+srv://sbsiddharth:sidrulzbatra1234@cluster0-vvlal.mongodb.net/test?retryWrites=true&w=majority', { keepAlive : true });
+
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb+srv://darksun27:sidrulzbatra1234@cluster0-vvlal.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, keepAlive : true, }).then(
+    () => {
+      console.log("Database connection established!");
+    },
+    err => {
+      console.log("Error connecting Database instance due to: ", err);
+    }
+  );;
 
+// var db = mongoose.connection;
 
-var db = mongoose.connection;
-
-db.once('open', ()=>{
-    console.log('Database Connected');
-})
+// db.once('open', ()=>{
+//     console.log('Database Connected');
+// })
 
 var walletSchema = new mongoose.Schema({
     username : { type: String },
     mobile_number : { type : Number, unique : true, required : true },
     card_details : Number,
-    amount : Number
+    amount : { type : Number, default : 0 }
 })
 
 var userHistory = new mongoose.Schema({
@@ -135,12 +142,21 @@ app.post("/api/spend", async (req, res)=> {
 });
 
 app.post("/api/add-amount", async (req, res)=> {
-    let transaction_details = {
+    let wallet_id = {
         mobile_number : req.body.mobile_number,
-        amount : req.body.amount,
     }
-
-    await Wallet.findOneAndUpdate({mobile_number : transaction_details['mobile_number']}, {amount : transaction_details['amount']}, (err, details)=>{
+    let amount_final = null;
+    await Wallet.find(wallet_id, (err, details)=> {
+        if(err) {
+            console.log("Error Fetching Wallet!");
+        }
+        else {
+            amount_final = details[0]['amount'];
+        }
+    });
+    amount_final += + parseInt(req.body.amount, 10);
+    console.log(amount_final)
+    await Wallet.findOneAndUpdate(wallet_id, {amount : amount_final}, (err, details)=>{
         if(err) {
             res.status(400);
         }
