0696df3d09176a047b54d7afef2c2025c2a0564f
darksun27 <sbsiddharth@gmail.com>
2020-01-25 16:56:36 +0530

    Remove Gibberish From Map

---
+++ remove_everythin_ex_map.js
@@ -0,0 +1,15 @@
+var element = document.getElementsByTagName("header");
+element[0].remove();
+element = document.getElementsByClassName("heart");
+element[0].remove();
+element = document.getElementsByTagName("footer");
+element[0].remove();
+element = document.getElementsByClassName("mapheader");
+element[0].remove();
+element = document.getElementsByClassName("allstores");
+element[0].remove();
+element = document.getElementsByClassName("in-page-push");
+element[0].style.padding = "0"
+element = document.getElementsByClassName("aims")
+element[0].style.margin = "0"
+element[0].style.padding = "0"
